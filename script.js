0d87d830066cdb8b77c0939a0ba76bd65c895bef
leonardo <leo.ramos.ari@gmail.com>
2019-12-03 15:26:30 -0500

    he crado mi primera pagina

---
+++ script.js
@@ -0,0 +1,22 @@
+/* var a = [];
+var cad = ""
+for (var j = 0; j<4;j++){
+    cad = cad + "1"
+    a[j] = cad;
+}
+
+console.log(a); */
+
+/* var a = [];
+var num = -1;
+var cad = "";
+
+for (var j = 0;j<4;j++){
+    for(var i = 0: i)
+}
+
+console.log(a); */
+
+
+
+
